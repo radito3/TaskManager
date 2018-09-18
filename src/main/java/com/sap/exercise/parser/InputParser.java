66acd81ce793e5dac3824e7f6b63fdb79663b3d4
Rangel Ivanov <radito3@mail.bg>
2018-09-18 17:00:11 +0300

    Work on input parser

--- src/main/java/com/sap/exercise/parser/InputParser.java
+++ src/main/java/com/sap/exercise/parser/InputParser.java
@@ -4,12 +4,9 @@ import com.sap.exercise.parser.commands.*;
 import com.sap.exercise.printer.OutputPrinter;
 
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class InputParser {
 
@@ -18,7 +15,7 @@ public class InputParser {
     private static List<Command> commands = Arrays.asList(
             new Exit(), new Add(), new Edit(), new Delete());
 
-    //there needs to be a OutputPrinter class variable
+    private static OutputPrinter printer = new OutputPrinter().configure(System.out);
 
     public static void run(InputStream in) {
         try (Scanner scanner = new Scanner(in)) {
@@ -28,27 +25,21 @@ public class InputParser {
                 List<String> args = inputArgs(input);
 
                 for (Command command : commands) {
-                    if (args.get(0).equals(command.getName())) { //may be .startsWith() but prefer not
+                    if (args.get(0).equals(command.getName())) {
                         command.execute();
                         continue outside;
                     }
                 }
 
-                new OutputPrinter().configure(System.out).print("Invalid command");
+                printer.print("Invalid command");
             }
+        } catch (Exception e) { //will specify the exception later
+            printer.print(e.getMessage());
+            run(in); //not the best solution
         }
     }
 
     private static List<String> inputArgs(String input) {
-        Pattern pattern = Pattern.compile("^(\\w+)\\s*$"); //doesn't work
-        Matcher matcher = pattern.matcher(input);
-        List<String> args = new ArrayList<>();
-        if (matcher.matches()) {
-            for (int i = 0; i < matcher.groupCount(); i++) {
-                args.add(matcher.group(i));
-            }
-        }
-        args.forEach(System.out::println);
-        return args;
+        return Arrays.asList(input.split("\\s+")); //doesn't work when a command is written after a series of \s characters (works with \r|\n)
     }
 }
