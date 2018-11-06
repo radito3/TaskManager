9e6c1fe2a9921537131c056c81cef28787a2734d
Rangel Ivanov <radito3@mail.bg>
2018-11-06 19:18:29 +0200

    Add missing parenthesis in OutputPrinter

--- src/main/java/com/sap/exercise/printer/OutputPrinter.java
+++ src/main/java/com/sap/exercise/printer/OutputPrinter.java
@@ -186,7 +186,7 @@ public class OutputPrinter {
     private Function<Calendar, Set<Event>> valueMapper(Set<Event> events) {
         return (date) ->
                 events.stream()
-                        .filter(event -> DateUtils.isSameDay(date, event.getTimeOf())
+                        .filter(event -> DateUtils.isSameDay(date, event.getTimeOf()))
                         .collect(Collectors.toSet());
     }
 
