07edac2f68d5912d088245ae8730d6d8f09e3763
Rangel Ivanov <radito3@mail.bg>
2018-12-01 18:14:56 +0200

    Add CRUDOperations tests

--- src/test/java/com/sap/exercise/handler/CRUDOperationsTest.java
+++ src/test/java/com/sap/exercise/handler/CRUDOperationsTest.java
@@ -1,13 +1,17 @@
 package com.sap.exercise.handler;
 
 import com.sap.exercise.AbstractTest;
+import com.sap.exercise.model.CalendarEvents;
 import com.sap.exercise.model.Event;
 import com.sap.exercise.model.User;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -80,6 +84,69 @@ public class CRUDOperationsTest extends AbstractTest {
                 "An event is returned with invalid title");
     }
 
-    //TODO add tests for remaining methods
+    @Test
+    @DisplayName("Getting object identified by id test")
+    public void getObjectByIdTest() {
+        User user = new User();
+        Serializable id = CRUDOperations.create(user);
+        user.setId((Integer) id);
 
+        User test = CRUDOperations.getObjById(User.class, (Integer) id);
+        assertEquals(user, test, "Object retrieved by Id doesn't match");
+        CRUDOperations.delete(user);
+    }
+
+    @Test
+    @DisplayName("Trying to get object with invalid id test")
+    public void invalidIdTest() {
+        assertThrows(NullPointerException.class,
+                () -> CRUDOperations.getObjById(User.class, -1),
+                "Getting object with invalid id doesn't throw error");
+    }
+
+    @Test
+    @DisplayName("Getting events in time frame test")
+    public void getEventsInTimeFrameTest() {
+        Event event = new Event("for_testing");
+        event.setToRepeat(Event.RepeatableType.DAILY);
+        event.setTimeOf(new GregorianCalendar(2010, 0, 1));
+        EventHandler.getInstance().create(event);
+
+        List<CalendarEvents> events = CRUDOperations.getEventsInTimeFrame("2010-01-01", "2010-01-30");
+        assertEquals(30, events.size(), "Events list doesn't contain all event repetitions");
+        EventHandler.getInstance().delete(event);
+    }
+
+    @Test
+    @DisplayName("Getting events in invalid time frame test")
+    public void invalidTimeFrameGetTest() {
+        assertEquals(0, CRUDOperations.getEventsInTimeFrame("2018-01-01", "2017-01-01").size(),
+                "Invalid time frame returns non-empty list");
+    }
+
+    @Test
+    @DisplayName("Deleting events in time frame test test")
+    @Disabled("Not working yet")
+    public void deleteEventsInTimeFrameTest() {
+        Event event = new Event("for_testing");
+        event.setToRepeat(Event.RepeatableType.DAILY);
+        event.setTimeOf(new GregorianCalendar(2010, 0, 1));
+
+        Serializable id = CRUDOperations.create(event);
+        CRUDOperations.delete(event);
+        EventHandler.getInstance().create(event);
+        event.setId((Integer) id + 1);
+
+        CRUDOperations.deleteEventsInTimeFrame(event, "2010-01-15", "2010-01-30");
+
+        List<CalendarEvents> events = CRUDOperations.getEventsInTimeFrame("2010-01-01", "2010-01-30");
+        assertEquals(15, events.size(), "Events list doesn't contain correct amount of event repetitions");
+        EventHandler.getInstance().delete(event);
+    }
+
+    @Test
+    @DisplayName("Deleting events in invalid time frame test")
+    public void invalidTimeFrameDeleteTest() {
+        //implement
+    }
 }
