3c3565248bbf8e35fe137e41f9547a19ba47e511
radito3 <radito3@mail.bg>
2019-05-23 16:32:16 +0300

    Fix calendar options validation

--- src/main/java/com/sap/exercise/commands/validator/PrintCalendarCommandValidator.java
+++ src/main/java/com/sap/exercise/commands/validator/PrintCalendarCommandValidator.java
@@ -3,7 +3,10 @@ package com.sap.exercise.commands.validator;
 import com.sap.exercise.util.ExceptionMessageHandler;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Option;
-import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.Arrays;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 public class PrintCalendarCommandValidator extends DefaultCommandValidator {
 
@@ -14,20 +17,33 @@ public class PrintCalendarCommandValidator extends DefaultCommandValidator {
     @Override
     public boolean isValid() {
         boolean commonCondition = super.isValid();
-        //TODO add filtering of 'help' flag
-        if (commonCondition && (cmd.getOptions().length > 2 || optionsSizeWithoutEvents() > 1)) {
+        if (commonCondition && checkOptionsLength()) {
             ExceptionMessageHandler.setMessage("Invalid number of arguments");
             return false;
         }
         return commonCondition;
     }
 
-    private int optionsSizeWithoutEvents() {
+    private boolean checkOptionsLength() {
+        Supplier<Stream<Option>> optionsStreamSupplier = () -> Arrays.stream(cmd.getOptions());
         Option withEvents = Option.builder("e")
                 .required(false)
                 .longOpt("events")
                 .desc("Display calendar with events highlighted")
                 .build();
-        return ArrayUtils.removeElement(cmd.getOptions(), withEvents).length;
+        Option help = Option.builder()
+                .longOpt("help")
+                .required(false)
+                .desc("Print command help")
+                .build();
+
+        boolean onlyHelp = optionsStreamSupplier.get()
+                .allMatch(option -> option.equals(help));
+        boolean optWithoutEvents = optionsStreamSupplier.get()
+                .filter(opt -> !opt.equals(withEvents))
+                .count() > 1;
+        boolean allOpts = optionsStreamSupplier.get().count() > 2;
+
+        return !onlyHelp & (optWithoutEvents | allOpts);
     }
 }
